acc44e4c29526f1804a0c0405eaf6d30bd13b224
Salavat Shirgaleev <salavatshirgaleev@MacBook-Air-Salavat.local>
2018-01-07 14:06:00 -0600

    Controller adding

--- springproject/src/main/java/com/sshirgaleev/springproject/bootstrap/DevBootstrap.java
+++ springproject/src/main/java/com/sshirgaleev/springproject/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package com.sshirgaleev.springproject.bootstrap;
 
 import com.sshirgaleev.springproject.model.Author;
 import com.sshirgaleev.springproject.model.Book;
+import com.sshirgaleev.springproject.model.Publisher;
 import com.sshirgaleev.springproject.repositories.AuthorRepository;
 import com.sshirgaleev.springproject.repositories.BookRepository;
+import com.sshirgaleev.springproject.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -14,10 +16,12 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -28,8 +32,13 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
 
     private void initData(){
+
+        Publisher publisher = new Publisher("Izi Rep");
+
+        publisherRepository.save(publisher);
+
         Author author1 = new Author("Salavat" ,"Shirgaleev" );
-        Book book1 = new Book("How to write ", "1234", "IziRep");
+        Book book1 = new Book("How to write ", "1234", publisher);
         author1.getBooks().add(book1);
         book1.getAuthors().add(author1);
 
@@ -38,7 +47,7 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
 
         Author author2 = new Author("Azamat" ,"Shirgaleev" );
-        Book book2 = new Book("Blya UUU ", "1235", "IziRep");
+        Book book2 = new Book("Blya UUU ", "1235", publisher);
         author2.getBooks().add(book2);
         book2.getAuthors().add(author2);
 
